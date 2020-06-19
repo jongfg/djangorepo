3772e00cee1480b8e4e9a277f6a618088212e673
jongfg <jmdevuser+gfg@gmail.com>
2020-06-19 16:19:59 -0700

    initial commit dictionary api call - requirements and technical spec

---
+++ dictionaryapi/getDefinition.py
@@ -0,0 +1,13 @@
+# author - JonM
+# topic - Django Python GeeksforGeeks
+# what - use Dictionary api to request definition for word and display on console
+# requirements - request user input for word, create request object for api, execute api and display response
+# technical specs - use dictionary.com api key (must register), Python3, Import Requests package, JSON payload
+
+# psuedo algorithm
+# import requests library
+# get input from user as text (word from dictionary)
+# create base and query URL for API (include api key and word input)
+# execute query against api and receive response data
+# convert data from json to object literal
+# display data on console
